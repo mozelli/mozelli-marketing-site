9f50fcecc7786432845fab95d4698f6be54033ed
João Mozelli Neto <joaomozelli@hotmail.com>
2026-02-23 14:17:32 -0300

    instala o adaptador da Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -10,4 +12,5 @@ export default defineConfig({
   },
 
   integrations: [],
+  adapter: vercel(),
 });
