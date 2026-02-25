97e286045ce405c37f10abf340389c533a8d567b
João Mozelli Neto <joaomozelli@hotmail.com>
2026-02-25 16:43:05 -0300

    otimizações no astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,14 +10,32 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.mozellimarketing.com.br",
+  // Força a geração de URLs amigáveis e consistentes
+  trailingSlash: "ignore",
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      // Melhora a minificação de CSS e JS
+      cssMinify: "lightningcss",
+      assetsInlineLimit: 4096, // Transforma ícones pequenos em Base64 para reduzir requests HTTP
+    },
   },
 
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      lastmod: new Date(),
+      changefreq: "weekly",
+      priority: 0.7,
+    }),
+  ],
   adapter: vercel({
     webAnalytics: {
       enabled: false,
+    },
+    imagesConfig: {
+      sizes: [320, 640, 1200],
+      domains: [],
+      formats: ["image/webp", "image/avif"],
     },
   }),
 });
